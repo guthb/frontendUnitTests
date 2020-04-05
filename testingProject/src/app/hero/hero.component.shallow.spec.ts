e3a8b6067f19321a4918b09d38dd0ee35e6b999d
Bradley Guthrie <guppy96@gmail.com>
2020-04-04 19:04:23 -0500

    created test using longhand provider service for injected service

--- testingProject/src/app/hero/hero.component.shallow.spec.ts
+++ testingProject/src/app/hero/hero.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing'
-import { HeroComponent } from '../hero/hero.component'
+import { HeroComponent } from './hero.component'
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 
 describe('HeroComponet (shallow test)', () => {
@@ -25,7 +26,13 @@ describe('HeroComponet (shallow test)', () => {
   it('should renter the hero name in an anchor tag', () => {
     fixture.componentInstance.hero = { id: 1, name: 'fred', strength: 4 };
     fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector('a').textContent).toContain('fred')
+
+    let deAnchor = fixture.debugElement.query(By.css('a'));
+    expect(deAnchor.nativeElement.textContent).toContain('fred');
+
+    //expect(fixture.debugElement.query(By.css('a')).nativeElement.textContent).toContain('fred'); //same
+
+    //expect(fixture.nativeElement.querySelector('a').textContent).toContain('fred') //same
   })
 
 })
